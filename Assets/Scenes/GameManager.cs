57d37208d62cba16a12d2751f251400b4f019e7b
Sugar_송환욱 <85896566+Ugee0810@users.noreply.github.com>
2023-01-27 14:20:26 +0900

    Update

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Sugar;
+
+public class GameManager : SugarBehaviour
+{
+    private void Awake()
+    {
+        MessageOutput(Message_001);
+    }
+}
